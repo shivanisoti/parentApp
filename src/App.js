61e2bd7f5efd9e42a35c00aca568276b355ef9c4
shivani-soti <shivani.soti@globant.com>
2024-01-19 15:09:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ParentApp from './ParentApp';
+
+function App() {
+  return (
+    <div className="App">
+      <ParentApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ParentApp.js
@@ -0,0 +1,32 @@
+import React, { useRef } from 'react';
+
+const ParentApp = () => {
+  const iframeRef = useRef(null);
+
+  const callIframeFunction = () => {
+    // Post a message to the iframe to trigger the function call
+    iframeRef.current.contentWindow.postMessage('callMyFunctionInIframe', 'https://iframe-auhk508wn-shivanis-projects-0c4ad5de.vercel.app');
+  };
+
+  // Listen for messages from the iframe
+  const handleMessage = (event) => {
+    console.log('event', event);
+    if (event.origin == 'https://iframe-auhk508wn-shivanis-projects-0c4ad5de.vercel.app' && event.data == 'FunctionExecuted') {
+      console.log('Function in iframe executed successfully');
+    }
+  };
+
+  // Attach the event listener
+  window.addEventListener('message', handleMessage);
+
+  return (
+    <div>
+      <h1>Parent App</h1>
+          <button onClick={callIframeFunction}>Call Iframe Function</button>
+          <br/>
+      <iframe ref={iframeRef} src="https://iframe-auhk508wn-shivanis-projects-0c4ad5de.vercel.app" title="Your Iframe"></iframe>
+    </div>
+  );
+};
+
+export default ParentApp;
